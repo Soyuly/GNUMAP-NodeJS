70371a5ebed2e8c5b8748738f124405fb9a18f5a
Soyuly <soyul8147@gmail.com>
2022-05-16 12:45:25 +0900

    db수정

--- db/index.js
+++ db/index.js
@@ -3,9 +3,9 @@ const host = "localhost";
 
 module.exports = {
   connection: mysql.createConnection({
-    host: '203.255.3.2463',
+    host: '203.255.3.246',
     user: "root",
-    password: "18classgnuamp",
+    password: "18classgnumap",
     database: "gnumap",
   }),
 };
