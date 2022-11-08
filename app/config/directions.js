8cea953650c97a21ff760e1225134dc9d0e8b4bc
dbstn1325 <dbstn1325@naver.com>
2022-11-08 22:00:31 +0900

    [fix] 소요거리 버그 수정

--- app/config/directions.js
+++ app/config/directions.js
@@ -49,8 +49,8 @@ function getDistanceAndTime(lat1, lng1, lat2, lng2) {
   distance = distance * 60 * 1.1515 * 1.609344 * 1000;
 
   // / 1000 => km 로 변환
-  if (distance < 100) distance = Math.round(distance / 10) * 10;
-  else distance = Math.round(distance / 100) * 100;
+  if (distance < 100) distance = Math.round((distance / 10) * 10);
+  else distance = Math.round((distance / 100) * 100);
 
   // 보행자 평균 속도: 4.644 km/h -> time이 너무 적게 나와서 수정 (4.644 km/h -> 3.644 km/h)
   // 분 단위로 시간 변환 ( * 60 )
