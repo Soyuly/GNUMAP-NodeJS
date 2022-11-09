cf91abc11c1d2f967c81405923410333b5674d03
soyuly <soyul8147@gmail.com>
2022-11-09 13:50:27 +0900

    [feature]슬랙 봇 추가

--- app/src/app/Revise/reviseController.js
+++ app/src/app/Revise/reviseController.js
@@ -1,11 +1,10 @@
-const jwtMiddleware = require("../../../config/jwtMiddleware");
 const reviseProvider = require("./reviseProvider");
 const reviseService = require("./reviseService");
 const baseResponse = require("../../../config/baseResponseStatus");
-const {response, errResponse} = require("../../../config/response");
-const regexEmail = require("regex-email");
-const {emit} = require("nodemon");
-const nodemailer = require("nodemailer");
+const { response, errResponse } = require("../../../config/response");
+
+const { WebClient } = require("@slack/web-api");
+const { emit } = require("nodemon");
 
 /**
  * API No. 1
@@ -13,22 +12,15 @@ const nodemailer = require("nodemailer");
  * [POST] /app/revise
  */
 exports.postRevise = async function (req, res) {
+  const { title, body } = req.body;
 
-    const {title, body} = req.body;
+  if (!title) return res.send(response(baseResponse.REVISE_TITLE_EMPTY));
 
-    if(!title)
-        return res.send(response(baseResponse.REVISE_TITLE_EMPTY));
+  if (!body) return res.send(response(baseResponse.REVISE_BODY_EMPTY));
 
-    if(!body)
-        return res.send(response(baseResponse.REVISE_BODY_EMPTY));
+  const reviseResponse = await reviseService.createRevise(title, body);
 
-
-    const reviseResponse = await reviseService.createRevise(
-        title,
-        body
-    );
-
-    return res.send(reviseResponse);
+  return res.send(reviseResponse);
 };
 
 /**
@@ -36,59 +28,26 @@ exports.postRevise = async function (req, res) {
  * API Name : revise 조회 API
  * [GET] /app/getRevise
  */
-exports.getRevise = async function(req, res) {
-    const reviseListResult = await reviseProvider.retrieveRevise();
-    return res.send(reviseListResult);
-}
-
+exports.getRevise = async function (req, res) {
+  const reviseListResult = await reviseProvider.retrieveRevise();
+  return res.send(reviseListResult);
+};
 
 /**
  * API No. 3
  * API Name : revise 메일 보내기 API
  * [POST] /app/postMail
  */
-exports.postMail = async function(req, res) {
-    const {title, body} = req.body;
+exports.postMail = async function (req, res) {
+  const { title, body } = req.body;
 
-    if(!title)
-        return res.send(response(baseResponse.REVISE_TITLE_EMPTY));
+  if (!title) return res.send(response(baseResponse.REVISE_TITLE_EMPTY));
 
-    if(!body)
-        return res.send(response(baseResponse.REVISE_BODY_EMPTY));
+  if (!body) return res.send(response(baseResponse.REVISE_BODY_EMPTY));
 
-    const reviseResponse = await reviseService.createRevise(
-        title,
-        body
-    );
+  const web = new WebClient(process.env.SLACK_BOT_TOKEN);
 
+  web.chat.postMessage({ channel: "그누맵", text: `*${title}*\n${body}` });
 
-    const email = {
-        "host" : "smtp.mailtrap.io",
-        port: 2525,
-        secure: false,
-        auth:{
-            user:"3d0e985b880725",
-            pass:"21f4c8f16f70f4"
-        }
-    }
-
-    const send = async(data) => {
-        nodemailer.createTransport(email).sendMail(data, function(error, info) {
-            if(error) {
-                console.log(error);
-            } else {
-                console.log("message 전송 완료");
-                return info.response;
-            }
-        });
-    };
-
-    const content = {
-        from: "gentleman959@gmail.com",
-        to : "208ede1b74-0e0eee@inbox.mailtrap.io",
-        subject: req.body.title,
-        html: req.body.body
-    };
-
-    res.send(content);
-}
+  res.send({ title: title, body: body });
+};
