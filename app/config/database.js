fa711729227e9f40f3c79417c9e90a5f2cf46f31
Soyul <soyul8147@gmail.com>
2023-03-02 03:27:54 +0900

    Update database.js

--- app/config/database.js
+++ app/config/database.js
@@ -1,14 +1,5 @@
 const mysql = require("mysql2/promise");
 
-// TODO: 본인의 DB 계정 입력
-// const pool = mysql.createPool({
-//   host: "203.255.3.66",
-//   port: 13306,
-//   user: "root",
-//   password: "5607",
-//   database: "gnumap",
-// });
-
 const pool = mysql.createPool({
   host: process.env.DEV_MYSQL_HOST,
   port: process.env.DEV_MYSQL_PORT,
